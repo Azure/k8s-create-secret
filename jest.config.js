d674adc7a1379205bbeda604ecb4f6c75bc4f0b4
raghaves <raghaves@microsoft.com>
2020-06-05 16:29:48 +0530

    Test coverage report

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-    clearMocks: true,
-    moduleFileExtensions: ['js', 'ts'],
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-    transform: {
-      '^.+\\.ts$': 'ts-jest'
-    },
-    verbose: true
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  verbose: true,
+  coverageThreshold: {
+    "global": {
+      "branches": 80,
+      "functions": 80,
+      "lines": 80,
+      "statements": 80
+    }
   }
+}
