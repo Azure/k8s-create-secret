4a67c2b9afb1b32bdb5c51baa5d8594245d4e447
rgsubh <64013610+rgsubh@users.noreply.github.com>
2020-06-12 16:34:22 +0530

    Updating README.md with version v1.1 and Test Coverage report (#8)
    * Updating README.md with version v1.1
    
    * Test coverage report
    
    * Test coverage report
    
    * Test coverage report
    
    * Review Comments fix

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-    clearMocks: true,
-    moduleFileExtensions: ['js', 'ts'],
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts'],
-    transform: {
-      '^.+\\.ts$': 'ts-jest'
-    },
-    verbose: true
+  clearMocks: true,
+  moduleFileExtensions: ['js', 'ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  verbose: true,
+  coverageThreshold: {
+    "global": {
+      "branches": 30,
+      "functions": 30,
+      "lines": 30,
+      "statements": 30
+    }
   }
+}
