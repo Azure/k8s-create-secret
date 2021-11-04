3a62c01735f4bcef1a3a4070645f2379902b2963
David Gamero <davidgamero@microsoft.com>
2021-11-04 18:50:11 -0400

    r

--- src/run.ts
+++ src/run.ts
@@ -202,7 +202,7 @@ async function run2() {
         console.log(`Failed to create secret with error: ${e}`)
         console.log(e)
         let body = e?.response?.body
-        console.log(`Response Body:`)
+        console.log(`Failed with response rody:`)
         console.log(body)
     }
 
