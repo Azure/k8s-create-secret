9224d9424202f9b1d22a0c809562c4572ce3f791
David Gamero <davidgamero@microsoft.com>
2021-11-04 18:35:45 -0400

    get client body

--- src/run.ts
+++ src/run.ts
@@ -201,6 +201,7 @@ async function run2() {
     } catch (e) {
         console.log(`Failed to create secret with error: ${e}`)
         console.log(e)
+        console.log(e.client?.body)
     }
 
     console.log(result)
