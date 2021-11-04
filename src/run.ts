a81da11304abc67cd0946862b56f081b5f51ef4c
David Gamero <davidgamero@microsoft.com>
2021-11-04 18:46:17 -0400

    e

--- src/run.ts
+++ src/run.ts
@@ -201,7 +201,8 @@ async function run2() {
     } catch (e) {
         console.log(`Failed to create secret with error: ${e}`)
         console.log(e)
-        console.log(e?.response?.body)
+        let body = e?.response?.body
+        console.log(`Response Body: ${body}`)
     }
 
     console.log(result)
